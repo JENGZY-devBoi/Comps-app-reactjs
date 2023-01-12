83106f5a504767589f4d4227e13fe1b2e58bdfa8
Teerapong Saisrikaew <teerapong.saisrikaew250819@gmail.com>
2023-01-12 16:01:57 +0700

    build variations to button: Button.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import Button from "./Button";
 function App() {
     return (
         <>
-            <Button success rounded outline>Click me!</Button>
-            <Button danger outline>Buy Now!</Button>
-            <Button secondary outline>Hide Ads!</Button>
+            <Button primary>button</Button>
+            <Button secondary>button</Button>
+            <Button success>button</Button>
+            <Button warning>button</Button>
+            <Button danger>button</Button>
+
+            <Button primary rounded>button</Button>
+            <Button secondary rounded>button</Button>
+            <Button success rounded>button</Button>
+            <Button warning rounded>button</Button>
+            <Button danger rounded>button</Button>
+
+            <Button primary outline>button</Button>
+            <Button secondary outline>button</Button>
+            <Button success outline>button</Button>
+            <Button warning outline>button</Button>
+            <Button danger outline>button</Button>
+
+            <Button primary rounded outline>button</Button>
+            <Button secondary rounded outline>button</Button>
+            <Button success rounded outline>button</Button>
+            <Button warning rounded outline>button</Button>
+            <Button danger rounded outline>button</Button>
         </>
     );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 function Button({ 
     children, 
@@ -10,8 +10,23 @@ function Button({
     outline,
     rounded
 }) {
+    const classes = classnames('px-8 py-2 border-2', {
+        'bg-blue-500 border-blue-500 text-white': primary,
+        'bg-gray-900 border-gray-900 text-white': secondary,
+        'bg-green-500 border-green-500 text-white': success,
+        'bg-yellow-400 border-yellow-400 text-white': warning,
+        'bg-red-500 border-red-500 text-white': danger,
+        'rounded-full': rounded,
+        'bg-transparent': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger,
+    });
+
     return (
-        <button className="bg-red-500">
+        <button className={classes}>
             {children}
         </button>
     );
